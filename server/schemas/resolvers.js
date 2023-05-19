4d5aa6d55ea6dbe766f2e16843194e713c5f7306
Cole <ce.kasabian@gmail.com>
2023-05-19 01:11:35 -0700

    started the changes to get graphql started

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,33 @@
+const { sign } = require("jsonwebtoken");
+const User = require("../models/User");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findOne({
+          _id: context.user._id,
+        });
+        return user;
+      } else {
+        throw new AuthenticationError("There is no user logged in");
+      }
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      const token = signToken(user);
+      return { user, token };
+    },
+  },
+};
+
+module.exports = resolvers;
